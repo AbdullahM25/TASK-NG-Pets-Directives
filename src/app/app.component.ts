f4122003efcbaf990c921b391d6737baeabae877
AbdullahM25 <AbdullahSMishari@gmail.com>
2025-04-07 10:11:07 +0300

    finished ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 
 import pets from '../data/pets';
 import { PetComponent } from './pet/pet.component';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [PetComponent],
+  imports: [PetComponent, NgFor, NgIf, CommonModule],
 })
 export class AppComponent {
   title = 'TASK-NG-Pets-Directives';
   pets = pets;
+  isAdopted: boolean = true;
 }
